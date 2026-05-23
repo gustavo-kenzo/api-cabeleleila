93a9bbb873ca3bd31929fb86169bb4489c67acf6
Gustavo <sgkenzo212007@gmail.com>
2026-05-23 17:17:15 -0300

    feat(client): add controller with registration and query endpoints

---
+++ src/main/java/br/com/dsin/cabeleleila/controller/ClientController.java
@@ -0,0 +1,35 @@
+package br.com.dsin.cabeleleila.controller;
+
+import br.com.dsin.cabeleleila.dto.register.ClientRegister;
+import br.com.dsin.cabeleleila.dto.response.ClientDetailResponse;
+import br.com.dsin.cabeleleila.dto.response.ClientResponse;
+import br.com.dsin.cabeleleila.service.ClientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("client")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<ClientResponse> registerClient(@RequestBody @Valid ClientRegister dto) {
+        return ResponseEntity.ok(clientService.registerClient(dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDetailResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.findById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<ClientResponse>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(clientService.findAll(pageable));
+    }
+}
